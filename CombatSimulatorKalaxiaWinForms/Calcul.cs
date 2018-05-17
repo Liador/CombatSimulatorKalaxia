ff8d213cabf946a3ccc841fe23525e1fa531fdf3
Liador <jean.lousberg@yahoo.fr>
2018-05-18 00:47:05 +0200

    Added Calcul.cs

---
+++ CombatSimulatorKalaxiaWinForms/Calcul.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+    static class Calcul
+    {
+        public static double Distance(int x0, int y0, int x1, int y1)
+        {
+            double distance;
+            distance = Math.Sqrt((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1));
+            return distance;
+        }
+    }
+}
